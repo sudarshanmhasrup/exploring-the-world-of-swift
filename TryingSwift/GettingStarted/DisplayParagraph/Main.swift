f4f23a34ba327b32af5ae2b8feb84422edb4607e
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-12-19 22:28:10 +0530

    Create a program to displaying a paragraph in Swift

---
+++ TryingSwift/GettingStarted/DisplayParagraph/Main.swift
@@ -0,0 +1,6 @@
+let paragraph = """
+Down in the sewer, the city felt different—darker, quieter, and forgotten.
+Water dripped from rusted pipes, echoing through narrow tunnels.
+It was a hidden world beneath the streets, unseen but always there.
+"""
+print(paragraph)
