89cea130a121e05a5b836e9258960ff7b61b97b5
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-12-19 21:34:20 +0530

    Add SwiftPlayground programs to the Package.swift as targets

--- Package.swift
+++ Package.swift
@@ -4,6 +4,8 @@ import PackageDescription
 let package = Package(
     name: "ExploringTheWorldOfSwift",
     targets: [
+        .executableTarget(name: "Playground", path: "SwiftPlayground/Playground/"),
+        .executableTarget(name: "Funground", path: "SwiftPlayground/Funground/"),
         .executableTarget(name: "HelloWorld", path: "TryingSwift/GettingStarted/HelloWorld/"),
         .executableTarget(name: "TypeInference", path: "TryingSwift/Variables/TypeInference/")
     ]
