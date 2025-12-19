36b8fe7ad4345d95eb6e5e51c7755f85e2229ce4
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-12-19 22:04:58 +0530

    Simplify "Main.swift" by removing redundant "main" function wrapping.

--- TryingSwift/Variables/TypeInference/Main.swift
+++ TryingSwift/Variables/TypeInference/Main.swift
@@ -1,13 +1,8 @@
-func main() {
-    // A dumb way because the Swift compiler can infer the data type of this variable
-    let name: String = "Sudarshan Mhasrup"
+// A dumb way because the Swift compiler can infer the data type of this variable
+let name: String = "Sudarshan Mhasrup"
 
-    // A smart way because the Swift compiler knows the data type of this variable
-    let role = "Mobile App Developer"
+// A smart way because the Swift compiler knows the data type of this variable
+let role = "Mobile App Developer"
 
-    print("Name: \(name).")
-    print("Role: \(role).")
-}
-
-// Calling the main function for execution of this program
-main()
+print("Name: \(name).")
+print("Role: \(role).")
