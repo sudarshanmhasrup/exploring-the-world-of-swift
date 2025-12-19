33263c27b543b5f27325c580aa1e3ac173baa394
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-12-19 22:08:42 +0530

    Add comments in the Package.swift to visually differentiate targets and modules

--- Package.swift
+++ Package.swift
@@ -4,9 +4,16 @@ import PackageDescription
 let package = Package(
     name: "ExploringTheWorldOfSwift",
     targets: [
+        // Swift Playground module programs
         .executableTarget(name: "Playground", path: "SwiftPlayground/Playground/"),
         .executableTarget(name: "Funground", path: "SwiftPlayground/Funground/"),
+
+        // Trying Swift module programs: Getting Started
         .executableTarget(name: "HelloWorld", path: "TryingSwift/GettingStarted/HelloWorld/"),
+
+        // Trying Swift module programs on the topic: Variables
+        .executableTarget(name: "ImmutableVariable", path: "TryingSwift/Variables/ImmutableVariable/"),
+        .executableTarget(name: "MutableVariable", path: "TryingSwift/Variables/MutableVariable/"),
         .executableTarget(name: "TypeInference", path: "TryingSwift/Variables/TypeInference/")
     ]
 )
